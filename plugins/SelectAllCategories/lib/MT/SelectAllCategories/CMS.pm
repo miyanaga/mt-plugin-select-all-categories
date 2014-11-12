f1c92c7952f6f0ccd7a3a8ee75dbc14599625ac9
Kunihiko Miyanaga <miyanaga@gmail.com>
2014-11-12 17:37:05 +0900

    Added selecting tree feature.

--- plugins/SelectAllCategories/lib/MT/SelectAllCategories/CMS.pm
+++ plugins/SelectAllCategories/lib/MT/SelectAllCategories/CMS.pm
@@ -15,12 +15,54 @@ sub template_param_edit_entry {
     $node->innerHTML(q!;
 <__trans_section component="SelectAllCategories">
 (function($) {
-    var $selectAll = $('<a href="#" style="float:left"><__trans phrase="Select All"></a>').click(function() {
+    var $selectAll = $('<a href="javascript:void(0)" style="float:left"><__trans phrase="Select All"></a>').click(function() {
             $('#category-selector-list .add-category-checkbox').prop('checked', true);
-        }), $unselectAll = $('<a href="#" style="float:left; margin-left: 8px"><__trans phrase="Unselect All"></a>').click(function() {
+            return false;
+        }), $unselectAll = $('<a href="javascript:void(0)" style="float:left; margin-left: 8px"><__trans phrase="Unselect All"></a>').click(function() {
             $('#category-selector-list .add-category-checkbox').prop('checked', false);
+            return false;
         });
     $('.category-selector-header').prepend($unselectAll).prepend($selectAll);
+
+    var hint = navigator.userAgent.indexOf('Mac') == -1 ?
+        '<__trans phrase="Press Ctrl key to select or unselect the tree.">' :
+        '<__trans phrase="Press command key to select or unselect the tree.">';
+    $('#category-selector-list').after($('<div class="hint" style="width:100%">' + hint + '</div>'));
+
+    $('input.add-category-checkbox').live('click',function(ev) {
+        if ( ev.metaKey ) {
+
+            var $listItem = $(this).closest('.list-item');
+            var baseChecked = $(this).prop('checked');
+            var $baseIndent = $listItem.children().first();
+            var baseIndent = parseInt($baseIndent.css('margin-left'));
+
+            var $current = $listItem.next();
+            while( $current && $current.length > 0 ) {
+
+                var $indent = $current.children().first();
+                var indent = parseInt($indent.css('margin-left'));
+                var $checkbox = $current.find('input.add-category-checkbox');
+                var checked = $checkbox.prop('checked');
+
+                if ( indent > baseIndent ) {
+                    if ( baseChecked == checked ) {
+                        // Do nothing
+                    } else {
+                        $checkbox.trigger('click');
+                    }
+                } else {
+                    // Finish
+                    break;
+                }
+
+                $current = $current.next();
+            }
+        }
+
+        return true;
+    });
+
 })(jQuery);
 </__trans_section>
     !);
--- plugins/SelectAllCategories/lib/MT/SelectAllCategories/L10N/ja.pm
+++ plugins/SelectAllCategories/lib/MT/SelectAllCategories/L10N/ja.pm
@@ -8,6 +8,8 @@ use vars qw( %Lexicon );
 %Lexicon = (
 	'Select All' => 'すべて選択',
 	'Unselect All' => 'すべて解除',
+    'Press Ctrl key to select or unselect the tree.' => 'Ctrlキーを押すと子カテゴリもまとめて選択/解除できます。',
+    'Press command key to select or unselect the tree.' => 'commandキーを押すと子カテゴリもまとめて選択/解除できます。',
 );
 
 1;
